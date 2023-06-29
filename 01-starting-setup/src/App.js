68fdd3f209c56112d746fb9ea5933ae02c590a25
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-06-29 17:33:39 +0100

    js vs react

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  // JS
+  // const para = document.createElement("p");
+  // para.textContent = "This is also visible !!!";
+  // document.getElementById("root").append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
