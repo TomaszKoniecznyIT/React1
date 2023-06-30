8ac265be830fc176ebf0c3a7f1e37d58fcc25cbf
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-06-30 11:44:25 +0100

    ExpenseDate - splitting components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
   //   return <h2>Expense items!</h2>;
   return (
     <div className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
