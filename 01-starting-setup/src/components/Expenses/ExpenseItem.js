d9ee5fd0f2f40f97f0ab72a92094c8a6c912ce58
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-06-30 15:24:42 +0100

    function syntax

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   //   return <h2>Expense items!</h2>;
   return (
     <Card className="expense-item">
@@ -14,6 +14,6 @@ function ExpenseItem(props) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
