edfdd59e8edd756bfb41834193c8cf1fa3103529
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-06 11:07:33 +0100

    state

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   //   return <h2>Expense items!</h2>;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Update");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
