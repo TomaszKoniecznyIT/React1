8d121832ae0c9818e40ef608e8c83988438e6d22
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-06 14:30:53 +0100

    add exoense form

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -30,8 +33,9 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      {/* <h2>Let's get started!</h2>
+      <p>This is also visible!</p> */}
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
