1a19d2d64fbe857b8d96add4194c4c96408f3b9e
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-10 10:03:42 +0100

    computed state

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -7,9 +7,19 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
+  const [filterInfoText, setFilterInfoText] = useState("2019, 2021 & 2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    if (selectedYear === "2019") {
+      setFilterInfoText("2020, 2021 & 2022");
+    } else if (selectedYear === "2020") {
+      setFilterInfoText("2019, 2021 & 2022");
+    } else if (selectedYear === "2021") {
+      setFilterInfoText("2019, 2020 & 2022");
+    } else {
+      setFilterInfoText("2019, 2020 & 2021");
+    }
   };
 
   return (
@@ -19,6 +29,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <p>Data for years {filterInfoText} is hidden.</p>
         <ExpenseItem
           date={props.items[0].date}
           title={props.items[0].title}
